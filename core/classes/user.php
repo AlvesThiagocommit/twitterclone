1da6a96709bddc846e94def6da7fc13ef8e69126
lobothiago <lobo.alvesthiago@gmail.com>
2017-12-01 16:34:11 -0200

    Criando função logout e userData

--- core/classes/user.php
+++ core/classes/user.php
@@ -30,5 +30,18 @@ class User{
             return false;
         }
     }
+
+    public function userData($user_id){
+        $stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `user_id` = :user_id");
+        $stmt->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchh(PDO::FETCH_OBJ);
+    }
+
+    public function logout(){
+        $_SESSION = array();
+        session_destroy();
+        header('Location: ../index.php');
+    }
 }
 ?>
